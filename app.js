c30bbf87cb2c3450f4aaf5178a59b7cdf1c8dec1
NH-Rifat <rifatr729@gmail.com>
2020-08-28 02:33:33 +0600

    'dfvfs'

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ let p2Score = document.querySelector("#p2score");
 const p1Button = document.querySelector("#p1btn");
 const p2Button = document.querySelector("#p2btn");
 
-//let GameInput = document.querySelector('#inputId')
+
+const reset = document.querySelector("#reset");
+let GameInput = document.querySelector('#inputId')
+
+let playScore = document.querySelector('#playScore')
+let resultShow = document.querySelector('#result')
+
 
 
 let scoreP1 = 0;
@@ -15,7 +21,19 @@ let gameover = false;
 
 //let winScore =  parseInt(GameInput.value) 
 //console.log(winScore)
-let winScore = 5;
+let winScore = 0;
+
+inputId.addEventListener('change', ()=>{
+  let inputValue = Number(inputId.value)
+  //console.log(inputValue);
+  playScore.innerText = inputValue
+  //console.log(playScore.innerText);
+  winScore = inputValue
+  inputId.value = ''
+  //console.log(winScore);
+
+})
+
 
 p1Button.addEventListener("click", () => {
   if (!gameover) {
@@ -24,11 +42,7 @@ p1Button.addEventListener("click", () => {
     p1Button.disabled = true;
     p2Button.disabled = false;
 
-    if (winScore === scoreP1) {
-      gameover = true;
-      p1Button.disabled = true;
-      p2Button.disabled = true;
-    }
+    scoreUpdate(scoreP1, winScore)
   }
 });
 
@@ -39,10 +53,40 @@ p2Button.addEventListener("click", () => {
     p1Button.disabled = false;
     p2Button.disabled = true;
 
-    if (winScore === scoreP2) {
+    scoreUpdate(scoreP2, winScore)
+  }
+});
+
+
+//Reapeating task using by unction
+  function scoreUpdate (oldScore, winScore){
+    if (winScore === oldScore) {
       gameover = true;
       p1Button.disabled = true;
       p2Button.disabled = true;
+      
+      if(scoreP1 ===winScore ){
+          resultShow.innerText='Congratulation! Player1 win the match'
+      }else if(scoreP2 ===winScore){
+        resultShow.innerText='Congratulation! Player2 win the match'
+      }
+
     }
   }
-});
+
+
+reset.addEventListener('click', ()=>{
+
+  scoreP1 = 0;
+  scoreP2 = 0;
+  p1Score.innerText = 0;
+  p2Score.innerText = 0;
+
+  p1Button.disabled = false;
+  p2Button.disabled = false;
+  gameover = false;
+
+  playScore.innerText = 0;
+
+})
+
